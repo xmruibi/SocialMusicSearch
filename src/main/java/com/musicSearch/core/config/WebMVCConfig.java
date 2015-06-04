768510ce87b46c9236018a5f0dbb43f922714ca3
xmruibi <xmruibi@gmail.com>
2015-06-04 00:47:33 -0400

    #1_Static_Resources_visit

--- src/main/java/com/musicSearch/core/config/WebMVCConfig.java
+++ src/main/java/com/musicSearch/core/config/WebMVCConfig.java
@@ -4,12 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.musicSearch.core.domain.Music;
 
-@EnableWebMvc
 @Configuration
-public class WebMVCConfig {
-
+public class WebMVCConfig extends WebMvcConfigurerAdapter {
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/static").setViewName("forward:/index.html");
+	}
 }
