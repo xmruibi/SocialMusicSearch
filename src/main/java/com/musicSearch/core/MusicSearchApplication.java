1ae733e478207afbff6a4e51bc19533bb19232b8
Rui Bi <xmruibi@gmail.com>
2015-06-09 15:05:10 -0400

    #1_deployed

--- src/main/java/com/musicSearch/core/MusicSearchApplication.java
+++ src/main/java/com/musicSearch/core/MusicSearchApplication.java
@@ -2,12 +2,17 @@ package com.musicSearch.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.musicSearch.core.config.*;
 
 
 /**
@@ -15,16 +20,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @author ruibi
  *
  */
-@SpringBootApplication
+@Configuration
 @EnableAutoConfiguration
 @Import(RepositoryRestMvcConfiguration.class)
-public class MusicSearchApplication {
+public class MusicSearchApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MusicSearchApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(
+				MusicSearchApplication.class, args);
+		//SpringApplication.run(MusicSearchApplication.class, args);
 	}
 
-	
+	@Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(MongoDBConfig.class,
+					ElasticSearchConfig.class, ApplicationConfig.class,
+					WebMVCConfig.class);
+	    }
+
 	@RequestMapping(value = "/status", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	String status() {
 		return "{\"state\": \"running\"}";
